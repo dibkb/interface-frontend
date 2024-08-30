820562c32c9cab4e4acc97aa6e89ba12816e04e5
dibkb <dibas9110@gmail.com>
2024-08-31 00:56:17 +0530

    dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,10 +9,13 @@ const Dashboard = () => {
   return (
     <div className="container mx-auto min-h-screen py-6">
       <p className="text-center text-indigo-700 font-semibold mb-6">Dashboard</p>
-      <section className="mb-6">
-        <SmallSummaryCard title="Total orders" count={3434} />
+      <section className="mb-6 grid gap-x-4 gap-y-4 md:grid-cols-2">
+        <SmallSummaryCard title="Total orders" count={3434} className="col-span-1" />
+        <SmallSummaryCard title="Tolerance rate breached" count={3434} />
+        <SmallSummaryCard title="Order & Payment Received" count={3434} />
+        <SmallSummaryCard title="Payment Received" count={3434} />
       </section>
-      <main className="grid grid-cols-2 gap-4">
+      <main className="grid md:grid-cols-2 gap-4">
         <SummaryChart />
         <ToleranceChart />
       </main>
--- components/dashboard-charts/SmallSummaryCard.tsx
+++ components/dashboard-charts/SmallSummaryCard.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Card, CardContent } from '../ui/card';
 import { ChevronRight } from '../svg/ChevronRight';
+import { cn } from '@/lib/utils';
 interface SmallSummaryCard {
   title: string;
   count: number;
+  className?: string;
 }
-export const SmallSummaryCard = ({ title, count }: SmallSummaryCard) => {
+export const SmallSummaryCard = ({ title, count, className }: SmallSummaryCard) => {
   return (
-    <Card className="border p-4 flex items-center justify-between gap-2 hover:cursor-pointer hover:bg-zinc-100">
+    <Card
+      className={cn(
+        'border p-4 flex items-center justify-between gap-2 hover:cursor-pointer hover:bg-zinc-100',
+        className
+      )}
+    >
       <div className="flex flex-col gap-2">
         <p className="text-xs font-medium text-zinc-500">{title}</p>
         <h2 className="text-4xl">{count}</h2>
