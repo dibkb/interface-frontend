93d400c351a2e03272ed56d45ae7015925c3926e
dibkb <dibas9110@gmail.com>
2024-08-31 22:42:50 +0530

    correct routing from dashbaord

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -29,10 +29,28 @@ const Dashboard = () => {
     <main className="container mx-auto min-h-screen py-6">
       <p className="text-center text-indigo-700 font-semibold mb-6">Dashboard</p>
       <section className="mb-6 grid gap-x-4 gap-y-4 md:grid-cols-2">
-        <SmallSummaryCard title="Total orders" count={totalOrders} className="col-span-1" />
-        <SmallSummaryCard title="Tolerance rate breached" count={totalTolerance} />
-        <SmallSummaryCard title="Order & Payment Received" count={totalOrderAndPayment} />
-        <SmallSummaryCard title="Payment Pending" count={paymentPending} />
+        <SmallSummaryCard
+          title="Total orders"
+          count={totalOrders}
+          className="col-span-1"
+          href="tableview/total-orders"
+        />
+
+        <SmallSummaryCard
+          title="Tolerance rate breached"
+          count={totalTolerance}
+          href="tableview/total-rate-breched"
+        />
+        <SmallSummaryCard
+          title="Order & Payment Received"
+          count={totalOrderAndPayment}
+          href="tableview/order-payment"
+        />
+        <SmallSummaryCard
+          title="Payment Pending"
+          count={paymentPending}
+          href="tableview/payment-pending"
+        />
       </section>
       <main className="grid md:grid-cols-2 gap-4">
         <SummaryChart />
--- components/dashboard-charts/SmallSummaryCard.tsx
+++ components/dashboard-charts/SmallSummaryCard.tsx
@@ -4,25 +4,29 @@ import React from 'react';
 import { Card } from '../ui/card';
 import { ChevronRight } from '../svg/ChevronRight';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
 interface SmallSummaryCard {
   title: string;
   count: number;
   className?: string;
+  href: string;
 }
-export const SmallSummaryCard = ({ title = '', count = 0, className }: SmallSummaryCard) => {
+export const SmallSummaryCard = ({ title = '', count = 0, className, href }: SmallSummaryCard) => {
   if (!title || !count) return null;
   return (
-    <Card
-      className={cn(
-        'border p-4 flex items-center justify-between gap-2 hover:cursor-pointer hover:bg-zinc-100',
-        className
-      )}
-    >
-      <div className="flex flex-col gap-2">
-        <p className="text-xs font-medium text-zinc-500">{title}</p>
-        <h2 className="text-4xl">{count}</h2>
-      </div>
-      <ChevronRight />
-    </Card>
+    <Link href={href}>
+      <Card
+        className={cn(
+          'border p-4 flex items-center justify-between gap-2 hover:cursor-pointer hover:bg-zinc-100',
+          className
+        )}
+      >
+        <div className="flex flex-col gap-2">
+          <p className="text-xs font-medium text-zinc-500">{title}</p>
+          <h2 className="text-4xl">{count}</h2>
+        </div>
+        <ChevronRight />
+      </Card>
+    </Link>
   );
 };
