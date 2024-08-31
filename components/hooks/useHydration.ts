0e05948b026abeeb8eb4f4bcf60683a9a721c69d
dibkb <dibas9110@gmail.com>
2024-08-31 12:48:37 +0530

    .

--- components/hooks/useHydration.ts
+++ components/hooks/useHydration.ts
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { hydrateRoot } from 'react-dom/client';
 
 export const useHydration = () => {
   const [hydrated, setHydrated] = useState(false);
